d92e6cbec0c7162110c80631d035db5b255b1c12
luqmanmiraj <luqmanmiraj42@gmail.com>
2023-02-18 23:53:22 +0500

    add more pages

--- src/teks/fr.js
+++ src/teks/fr.js
@@ -1,7 +1,19 @@
 const fr = {
     "Architecture": { title: "", url: "", des: " " },
     'Web Vitals': { title: '', url: 'https://www.contentkingapp.com/academy/core-web-vitals/#tools', des: '' },
-
+    'jquery': { title: '', url: '', des: '' },
+    'CSS': { title: '', url: '', des: '' },
+    'HTML': { title: '', url: '', des: '' },
+    'sass': { title: '', url: '', des: '' },
+    'Tailwind': { title: '', url: '', des: '' },
+    'BootStrap': { title: '', url: '', des: '' },
+    'DOM': { title: '', url: '', des: '' },
+    'Vue': { title: '', url: '', des: '' },
+    'Angular': { title: '', url: '', des: '' },
+    'Bundler': { title: '', url: '', des: '' },
+    'w3c': { title: '', url: '', des: '' },
+    'web3': { title: '', url: '', des: '' },
+    'Tools': { title: '', url: '', des: '' },
 
 }
 
